88c3872cd7c4d34472fc8be5d65815f2344e4bc4
bonganimasemola <bonganimasemola@icloud.com>
2024-01-17 20:23:31 +0300

    finally figured it out

--- server/app.py
+++ server/app.py
@@ -1,83 +1,70 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
-from flask_restful import Api, Resource, reqparse
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+
+from models import db, Plant
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.json.compact = False
 
-db = SQLAlchemy()
 
-class Plant(db.Model, SerializerMixin):
-    __tablename__ = 'plants'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    image = db.Column(db.String)
-    price = db.Column(db.Float)
-
-    def __init__(self, name, image, price):
-        self.name = name
-        self.image = image
-        self.price = price
+db.init_app(app)  
 
 migrate = Migrate(app, db)
-db.init_app(app)
-
 api = Api(app)
 
-parser = reqparse.RequestParser()
-parser.add_argument('name', type=str, required=True, help='Name cannot be blank')
-parser.add_argument('image', type=str, required=True, help='Image cannot be blank')
-parser.add_argument('price', type=float, required=True, help='Price cannot be blank')
+class Index(Resource):
+
+    def get(self):
+        response_dict = {
+            "index": "Welcome to the REST Create and Retrieve Lab",
+        }
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+
+        return response
+
+api.add_resource(Index, '/')
 
 class Plants(Resource):
+
     def get(self):
-        try:
-            plants = Plant.query.all()
-            return [plant.to_dict() for plant in plants], 
-        except Exception as e:
-            return {'error': str(e)}, 500
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(jsonify(plants), 200)
 
     def post(self):
-        # args = parser.parse_args()
-        # new_plant = Plant(name=args['name'], image=args['image'], price=args['price'])
-        # OR
-        # import pdb
-        # pdb.set_trace()
-        args = self.parser.parse_args()
-        name = args.get('name')
-        image = args.get('image')
-        price = args.get('price')
-        new_plant = Plant(name=name, image=image, price=price)
-        
+        new_plant = Plant(
+            name=request.form['name'],
+            image=request.form['image'],
+            price=request.form['price'],
+        )
 
-        try:
-            db.session.add(new_plant)
-            db.session.commit()
-            return {"new_plant" :{
-                'name': new_plant.name,
-                'image': new_plant.image,
-                'price': new_plant.price
-            }}, 201
-        except Exception as e:
-            db.session.rollback()
-            return {'error': str(e)}, 500
-        finally:
-            db.session.close()
+        db.session.add(new_plant)
+        db.session.commit()
 
-class PlantByID(Resource):
-    def get(self, plant_id):
-        plant = Plant.query.get(plant_id)
-        if plant:
-            return plant.to_dict()
-        else:
-            return {'error': 'Plant not found'}, 404
+        response_dict = new_plant.to_dict()  # Fix the typo here
+
+        response = make_response(jsonify(response_dict), 201)
+
+        return response
 
 api.add_resource(Plants, '/plants')
-api.add_resource(PlantByID, '/plants/<int:plant_id>')
+
+class PlantByID(Resource):
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+        return make_response(jsonify(plant), 200)
+
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
